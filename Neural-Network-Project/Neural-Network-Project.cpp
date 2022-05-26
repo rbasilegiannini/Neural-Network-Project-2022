373c7f1ae3c85e75398910b3fc0485c795c56b23
rbasilegiannini <91531658+rbasilegiannini@users.noreply.github.com>
2022-05-26 16:27:05 +0200

    Start Part A

---
+++ Neural-Network-Project/Neural-Network-Project.cpp
@@ -0,0 +1,12 @@
+// Neural-Network-Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+using namespace boost::numeric::ublas;
+
+int main() {
+
+}
