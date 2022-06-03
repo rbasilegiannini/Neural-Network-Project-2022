69c7d4169ea78d6272d3842af054c83dafce53c9
rbasilegiannini <91531658+rbasilegiannini@users.noreply.github.com>
2022-06-03 16:20:09 +0200

    [FIX] Correct wrong indeces in BP algorithm

--- Neural-Network-Project/BackPropagation.cpp
+++ Neural-Network-Project/BackPropagation.cpp
@@ -5,10 +5,10 @@ using std::deque;
 
 vector<vector<Real>> BackPropagation::BProp(const DataFromNetwork& dataNN, const ErrorFuncType EFuncType, const vector<Real>& targets) {
 	
-	deque<deque<Real>> allDelta;
+	deque<vector<Real>> allDelta;
 
 	//	Compute the output delta
-	deque<Real> deltaOutput;
+	vector<Real> deltaOutput;
 
 	AFuncType AFuncType = dataNN.AFunctionDerivativePerLayer.back();
 	for (size_t k{ 0 }; k < dataNN.numNeuronsPerLayer.back(); k++) {
@@ -19,7 +19,7 @@ vector<vector<Real>> BackPropagation::BProp(const DataFromNetwork& dataNN, const
 		auto AFuncDer_k = ActivationFunction::AFunctionDerivative[AFuncType];
 		auto EFuncDer_k = ErrorFunction::EFunctionDer_RespectOutput[EFuncType];
 
-		deltaOutput.push_front(AFuncDer_k(a_k) * EFuncDer_k(y_k, t_k));
+		deltaOutput.push_back(AFuncDer_k(a_k) * EFuncDer_k(y_k, t_k));
 	}
 
 	allDelta.push_front(deltaOutput);
@@ -27,51 +27,34 @@ vector<vector<Real>> BackPropagation::BProp(const DataFromNetwork& dataNN, const
 	//	Compute the internal delta
 
 	//	From the LAST-1 layer to the first layer.
-	for (int i = (int)(dataNN.numLayers - 1) - 1; i > -1; i--) {
+	for (int layer = (int)(dataNN.numLayers - 1) - 1; layer > -1; layer--) {
 
-		deque<Real> delta_i;
-
-		/** DEBUG */
+		vector<Real> delta_i;
 		allDelta.push_front(delta_i);	// delta_i is empty. 
-		/**  */
 
-		AFuncType = dataNN.AFunctionDerivativePerLayer[i];
+		AFuncType = dataNN.AFunctionDerivativePerLayer[layer];
 
 		//	For all neurons of the layer i
-		for (size_t j{ 0 }; j < dataNN.numNeuronsPerLayer[i]; j++) {	
+		for (size_t idxNeuron{ 0 }; idxNeuron < dataNN.numNeuronsPerLayer[layer]; idxNeuron++) {	
 
-			//auto a_i = dataNN.activationsPerLayer[i][j];
-			auto a_i = dataNN.activationsPerLayer[i](j, 0); 
+			auto a_i = dataNN.activationsPerLayer[layer](idxNeuron, 0); 
 			auto AFuncDer_i = ActivationFunction::AFunctionDerivative[AFuncType];
 
-			//	Compute the summation, for all the neurons of the next layer (connected to the node i because of the NN is FF)
+			//	Compute the summation, for all the neurons of the next layer
 			Real summation{ 0 };
-			for (size_t h{ 0 }; h < dataNN.numNeuronsPerLayer[i + 1]; h++) {
-			//D	summation += dataNN.parameters[i + 1](h, i) * allDelta[i + 1][h];
-
-				/** DEBUG */
-				auto data = dataNN.parameters[i + 1](h, i);
-				auto data2 = allDelta[i + 1][h];
-				summation += data * data2;
-				/**  */
-			}
-
-			delta_i.push_front(AFuncDer_i(a_i) * summation);
+			for (size_t forwardConnection{ 0 }; forwardConnection < dataNN.numNeuronsPerLayer[layer + 1]; forwardConnection++)
+				summation += dataNN.parameters[layer + 1](forwardConnection, idxNeuron) * allDelta[layer + 1][forwardConnection];
+			
+			delta_i.push_back(AFuncDer_i(a_i) * summation);
 		}
 
-		//D allDelta.push_front(delta_i);
-
-		/** DEBUG */
-		allDelta[i] = delta_i;	//	Now delta_i is filled
-		/**  */
+		allDelta[layer] = delta_i;	//	Now delta_i is filled.
 	}
 	
 	//	Convert result in a vector of vectors of reals
 	vector<vector<Real>> allDelta_vector;
 	for (const auto& d_i : allDelta) {
-		vector<Real> temp;
-		for (const auto& d_ij : d_i) temp.push_back(d_ij);
-		allDelta_vector.push_back(temp);
+		allDelta_vector.push_back(d_i);
 	}
 	
 	return allDelta_vector;
