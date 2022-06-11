900e084933c4a61dfe813b73d7ccbcdbaff6e870
Roberto Basile Giannini <rbg94@hotmail.it>
2022-06-11 02:41:01 +0200

    Load dataset and compose TS, VS and TEST SET

--- Neural-Network-Project/main.cpp
+++ Neural-Network-Project/main.cpp
@@ -17,36 +17,53 @@
 #include "NeuralNetworkManager.h"
 #include "ReadMNIST.h"
 
+#include <array>
+#include <algorithm>
+#include <random>
+
+using std::default_random_engine;
+using std::random_device;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::array;
 using boost::numeric::ublas::matrix;
 using boost::numeric::ublas::subrange;
 using namespace std::chrono;
+using std::uniform_int_distribution;
 
 int main() {
 
-	string imagesFiles = "C:\\Users\\RBG94\\Desktop\\Progetto reti\\train-images.idx3-ubyte";
-	string labelsFile = "C:\\Users\\RBG94\\Desktop\\Progetto reti\\train-labels.idx1-ubyte";
+	string sampleImagesFile = "C:\\Users\\RBG94\\Desktop\\Progetto reti\\train-images.idx3-ubyte";
+	string sampleLabelsFile = "C:\\Users\\RBG94\\Desktop\\Progetto reti\\train-labels.idx1-ubyte"; 
+	string testImagesFile = "C:\\Users\\RBG94\\Desktop\\Progetto reti\\t10k-images.idx3-ubyte";
+	string testLabelsFile = "C:\\Users\\RBG94\\Desktop\\Progetto reti\\t10k-labels.idx1-ubyte";
 
-	auto samples = ReadSample(imagesFiles, labelsFile, 10000);
+	random_device rd;
+	unsigned seed = rd();
+	auto rng = default_random_engine(seed);
+	uniform_int_distribution<int> uDist_train(0, 59999);
+	uniform_int_distribution<int> uDist_test(0, 9999);
 
-	for (const auto& sample : samples) {
+	//	Compose the Training Set and the Validation Set
+	vector<ImageLabeled> samples = ReadSample(sampleImagesFile, sampleLabelsFile, 60000);
+	shuffle(samples.begin(), samples.end(), rng);
 
-		cout << "IMAGE: " << endl << endl;
-		for (const auto& row : RangeGen(0, sample.image.size1())) {
-			for (const auto& col : RangeGen(0, sample.image.size2())) {
-				int temp = sample.image(row, col);
-				if (temp > 0)
-					cout << 1;
-				else
-					cout << " ";
-			}
-			cout << endl;
-		}
-		cout << endl << "LABEL: " << (unsigned)sample.label << endl << endl;
-	}
+	array<ImageLabeled, 5000> trainingSet;
+	for (auto& s : trainingSet)
+		s = samples[uDist_train(rng)];
+
+	array<ImageLabeled, 2500> validationSet;
+	for (auto& s : validationSet)
+		s = samples[uDist_train(rng)];
+
+	//	Compose the Test Set
+	vector<ImageLabeled> tests = ReadSample(testImagesFile, testLabelsFile, 10000);
+	shuffle(tests.begin(), tests.end(), rng);
+
+	array<ImageLabeled, 2500> testSet;
+	for (auto& s : testSet)
+		s = tests[uDist_test(rng)];
 
 	cout << endl;
-
 }
